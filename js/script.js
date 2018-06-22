79d30ed89237c210fba239dbc30f7c2d8d8caf49
zyng <kisielewskimichal@gmail.com>
2018-06-22 16:30:35 +0200

    add project html+css+js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*jslint browser: true*/
+/*global $, document*/
+
+
+$(document).ready(function () {
+    'use strict';
+
+    var hamburger = document.querySelector('.menu-toggle'),
+        menuContent = document.querySelector('.container-menu'),
+        pageMenu = document.querySelector('.page-menu'),
+        hamburgerIcon = document.getElementById('hamburger-icon'),
+        slider = document.querySelector('.container-slider');
+
+
+    hamburger.addEventListener('click', function () {
+
+        pageMenu.classList.toggle('shadow-box');
+
+        if (menuContent.style.display === "block") {
+            menuContent.style.display = "none";
+            hamburgerIcon.src = "img/hamburger-close.png";
+            slider.style.display = "block";
+            pageMenu.style.height = "auto";
+        } else {
+            menuContent.style.display = "block";
+            hamburgerIcon.src = "img/hamburger-open.png";
+            slider.style.display = "none";
+            pageMenu.style.height = "100%";
+        }
+
+    });
+
+});
